bce418cf0c9e949584fe740b2a7d7bab710012fb
Danilo Tavares <danilobtavares2@gmail.com>
2026-03-02 22:54:33 -0300

    voltei pro antigo

--- public/app.js
+++ public/app.js
@@ -1,60 +1,136 @@
 let multasSalvas = []
-let multaSelecionada = null
 
 function goTo(id) {
     document.querySelectorAll('.screen').forEach(screen => {
-        screen.classList.remove('active')
-    })
-    document.getElementById(id).classList.add('active')
+        screen.classList.remove('active');
+    });
+
+    document.getElementById(id).classList.add('active');
 }
 
 const API_URL = "https://tcc-senai-iub1.onrender.com"
 
-/* ================= LOGIN ================= */
 
 async function fazerLogin() {
 
     let cpf = document.getElementById("cpf").value
     const senha = document.getElementById("senha").value
 
-    cpf = cpf.replace(/\D/g, "")
+    try {
 
-    if (cpf.length !== 11) {
-        alert("CPF inválido.")
+        cpf = cpf.replace(/\D/g, "")
+
+        if (cpf.length !== 11) {
+        alert("CPF inválido. O CPF deve conter 11 números.")
         return
     }
 
-    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
-    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
-    cpf = cpf.replace(/(\d{3})(\d{1,2})$/, "$1-$2")
-
-    try {
+        cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
+        cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
+        cpf = cpf.replace(/(\d{3})(\d{1,2})$/, "$1-$2")
+        
         const resposta = await fetch(`${API_URL}/login`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ cpf, senha })
-        })
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        cpf: cpf,
+        senha: senha
+    })
+})
 
         const dados = await resposta.json()
 
         if (dados.sucesso) {
+
+            alert("Login realizado com sucesso!")
+
+            console.log("Multas:", dados.multas)
+
             mostrarMultas(dados.multas)
+
+            // Aqui você pode atualizar a tela com os dados reais
             goTo("dashboard")
+
         } else {
+
             alert(dados.mensagem)
+
         }
 
     } catch (erro) {
+
         alert("Erro ao conectar com o servidor")
+        console.error(erro)
+
     }
 }
 
-/* ================= MOSTRAR MULTAS ================= */
+async function fazerRegistro() {
+
+    const nome = document.getElementById("nome").value
+    const sobrenome = document.getElementById("sobrenome").value
+    let cpf = document.getElementById("cpfRegistro").value
+    const senha = document.getElementById("senhaRegistro").value
+
+    cpf = cpf.replace(/\D/g, "")
+
+    if (cpf.length !== 11) {
+    alert("CPF inválido. O CPF deve conter 11 números.")
+    return
+}
+    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
+    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
+    cpf = cpf.replace(/(\d{3})(\d{1,2})$/, "$1-$2")
+
+    if (senha.length < 8) {
+    alert("A senha deve ter no mínimo 8 caracteres")
+    return
+}
+
+    try {
+
+        const resposta = await fetch(`${API_URL}/registro`, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        nome: nome,
+        sobrenome: sobrenome,
+        cpf: cpf,
+        senha: senha
+    })
+})
+
+        const dados = await resposta.json()
+
+        alert(dados.mensagem)
+
+        if (dados.sucesso) {
+        goTo("login")
+    }
+
+    } catch (erro) {
+        alert("Erro ao registrar")
+        console.error(erro)
+    }
+}
 
 function mostrarMultas(multas) {
 
     const lista = document.getElementById("listaMultas")
     lista.innerHTML = ""
+
+    if (!multas || multas.length === 0) {
+    lista.innerHTML = `
+        <div class="sem-multa">
+            Nenhuma multa encontrada
+        </div>
+    `
+    return
+}
 
     multasSalvas = multas
 
@@ -62,36 +138,37 @@ function mostrarMultas(multas) {
 
         const container = document.createElement("div")
 
-        container.innerHTML = `
-            <div class="card">
-                <h3>${multa.placa}</h3>
-                <p><strong>Renavam:</strong> ${multa.renavam}</p>
-                <p><strong>Motivo:</strong> ${multa.motivo}</p>
-                <p><strong>Data:</strong> ${new Date(multa.data).toLocaleDateString("pt-BR")}</p>
+container.innerHTML = `
+    <!-- CARD VEÍCULO -->
+    <div class="card">
+        <h3>${multa.placa}</h3>
 
-                <button class="btn-detalhes" onclick="abrirDetalhes(${index})">
-                    Ver detalhes >
-                </button>
-            </div>
+        <p><strong>Renavam:</strong> ${multa.renavam}</p>
+        <p><strong>Motivo:</strong> ${multa.motivo}</p>
+        <p><strong>Data:</strong> ${new Date(multa.data).toLocaleDateString("pt-BR")}</p>
 
-            <div class="card">
-                <h3>Custos:</h3>
-                <p>Guincho: <strong>R$ ${multa.guincho.toFixed(2)}</strong></p>
-                <p>Diárias: <strong>R$ ${multa.diarias.toFixed(2)}</strong></p>
-                <p>Total: <strong>R$ ${(multa.guincho + multa.diarias).toFixed(2)}</strong></p>
+        <button class="btn-detalhes" onclick="abrirDetalhes(${index})">
+            Ver detalhes >
+        </button>
+    </div>
 
-                <button class="btn-regularizar"
-                    onclick="abrirRegularizacao(${index})">
-                    Regularizar
-                </button>
-            </div>
-        `
+    <!-- CARD CUSTOS -->
+    <div class="card">
+    <h3>Custos:</h3>
 
-        lista.appendChild(container)
+    <p>Guincho: <strong>R$ ${multa.guincho.toFixed(2)}</strong></p>
+    <p>Diárias: <strong>R$ ${multa.diarias.toFixed(2)}</strong></p>
+    <p>Total: <strong>R$ ${(multa.guincho + multa.diarias).toFixed(2)}</strong></p>
+
+    <button class="btn-regularizar">
+        Regularizar
+    </button>
+</div>
+`
+
+lista.appendChild(container)
     })
 }
-
-/* ================= DETALHES ================= */
 
 function abrirDetalhes(index) {
 
@@ -104,52 +181,22 @@ function abrirDetalhes(index) {
     document.getElementById("detalheStatus").innerText = multa.motivo
     document.getElementById("detalheMotivo").innerText = multa.motivo
 
+    const dataFormatada =
+        new Date(multa.data).toLocaleDateString("pt-BR") +
+        " às " +
+        new Date(multa.data).toLocaleTimeString("pt-BR", {
+            hour: "2-digit",
+            minute: "2-digit"
+        })
+
+    document.getElementById("detalheData").innerText = "Data: " + dataFormatada
+
     goTo("detalhes")
 }
 
-/* ================= REGULARIZAÇÃO ================= */
-
-function abrirRegularizacao(index) {
-
-    multaSelecionada = multasSalvas[index]
-
-    const total = multaSelecionada.guincho + multaSelecionada.diarias
-
-    document.querySelector("#regularizacao .linha-info strong:nth-child(2)").innerText =
-        `R$ ${multaSelecionada.guincho.toFixed(2)}`
-
-    document.querySelectorAll("#regularizacao .linha-info strong")[1].innerText =
-        `R$ ${multaSelecionada.diarias.toFixed(2)}`
-
-    document.querySelectorAll("#regularizacao .linha-info strong")[2].innerText =
-        `R$ ${total.toFixed(2)}`
-
-    goTo("regularizacao")
-}
-
-/* ================= PAGAMENTO ================= */
-
-function selecionarPagamento() {
-
-    const tipo = document.querySelector('input[name="pagamento"]:checked').value
-
-    document.getElementById("areaCartao").style.display =
-        tipo === "cartao" ? "block" : "none"
-
-    document.getElementById("areaPix").style.display =
-        tipo === "pix" ? "block" : "none"
-}
-
-function confirmarPagamento() {
-    alert("Pagamento realizado com sucesso!")
-    goTo("sucesso")
-}
-
-/* ================= CPF ================= */
-
 function formatarCPF(input) {
 
-    let valor = input.value.replace(/\D/g, "")
+    let valor = input.value.replace(/\D/g, "") // remove tudo que não for número
 
     if (valor.length > 11) {
         valor = valor.slice(0, 11)
